9e2f57672b50b7676268046ce8c1e9e4e986261c
BekirYldrm <bekiryildirim.2508@gmail.com>
2024-01-10 01:24:00 +0300

    API parameters have been created and the response is returned as json.

--- placesNearby.js
+++ placesNearby.js
@@ -1,11 +1,33 @@
 import { Router } from "express";
-import {Client} from "@googlemaps/google-maps-services-js";
+import { Client } from "@googlemaps/google-maps-services-js";
+import { config } from "dotenv";
+import bodyParser from "body-parser";
+
+config();
+
+
 const client = new Client({});
 
 const router = Router();
 
 router
-    .get("/placesNearby",(req,res)=>{
-    res.send("Hellooo")});
+    .use(bodyParser.urlencoded({ extended: true }))
+
+
+    .get("/placesNearby", (req, res) => {
+
+        client
+            .placesNearby({
+                params: {
+                    location: { latitude: 33.8670522, longitude: 151.1957362 },
+                    radius: 1500,
+                    key: process.env.API_KEY,
+                }
+            })
+            .then(apiRes => {
+                res.json(apiRes.data.results)
+            })
+            .catch(error => console.error(error))
+    })
 
 export default router;
