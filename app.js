e8e535a3f479654748ff5bcfeda7b1f3de9004d3
BekirYldrm <bekiryildirim.2508@gmail.com>
2024-01-10 01:36:37 +0300

    connection created to database on remote server.

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 import express from "express";
-import { config } from "dotenv";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import placesNearby from "./placesNearby.js"
 
+
+
 const app = express();
 const PORT = 8070 || process.env.PORT
 
 app.
-    use(bodyParser.urlencoded({extended:true}))
+    use(bodyParser.urlencoded({ extended: true }))
     .use(cors())
 
-    .get("/placesNearby" , placesNearby )
+    .get("/placesNearby", placesNearby)
 
+mongoose
+    .connect(process.env.DB_URL)
 
-    .listen(PORT, ()=>{
-        console.log(`Server is running : ${PORT}`);
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running : ${PORT}`);
+        })
     })
+
+    .catch(error=>console.error(error));
+
+
